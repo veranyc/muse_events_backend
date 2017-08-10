2787d9de080366382039707805ec334fcb25ea98
veranyc <veranyc@gmail.com>
2017-08-10 14:45:06 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
     namespace :v1 do
       post '/login', to: 'auth#create'
       get '/me', to: 'auth#show'
+      resources :artists
+      resources :events
+      resources :pictures
+      resources :posts
+      resources :setlist_songs
+      resources :setlists
+      resources :songs
+      resources :users
     end
   end
 end
