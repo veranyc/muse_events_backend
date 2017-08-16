bcbbdeee7cb68dd4e78f9c3657ee2e6411f42e6f
veranyc <veranyc@gmail.com>
2017-08-16 10:51:45 -0400

    heroku update

--- app/serializers/song_serializer.rb
+++ app/serializers/song_serializer.rb
@@ -1,5 +1,5 @@
 class SongSerializer < ActiveModel::Serializer
-  attributes :id, :song_id, :setlist_id
+  attributes :id, :setlist_id
 
   belongs_to :artist
   has_many :setlists, through: :setlist_songs
