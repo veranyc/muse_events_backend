d7188f4ce12cd6a6bf88c274227d0696c78dd16d
veranyc <veranyc@gmail.com>
2017-08-16 11:50:03 -0400

    updated event serializer

--- app/serializers/event_serializer.rb
+++ app/serializers/event_serializer.rb
@@ -2,6 +2,7 @@ class EventSerializer < ActiveModel::Serializer
   attributes :id, :artist_id, :venue, :city, :state, :country, :notes, :date, :user_id
 
   belongs_to :user
+  has_many :setlists
   has_many :setlist_songs, through: :setlists
   has_many :posts
   has_many :pictures
