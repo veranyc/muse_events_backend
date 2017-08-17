60706e1741182780b5ca1b7ef27d2613d7467332
veranyc <veranyc@gmail.com>
2017-08-17 13:58:44 -0400

    auth updates

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -2,27 +2,19 @@ class Api::V1::AuthController < ApplicationController
   before_action :authorize_user!, only: [:show]
 
   def show
-    render json: {
-      id: current_user.id,
-      username: current_user.username
-    }
+    render json: current_user
   end
 
   def create
-    # see if there is a user with this username
     user = User.find_by(username: params[:username])
-    # if there is, make sure that they have the correct password
     if user.present? && user.authenticate(params[:password])
-      # if they do, render back a json response of the user info
-      # issue token
-      # created_jwt = issue_token({id: user.id})
-      # render json: {username: user.username,jwt: created_jwt}
-      render json: {username: user.username}
+      created_jwt = issue_token({id: user.id})
+      render json: {username: user.username, jwt: created_jwt}
     else
-      # otherwise, render back some error response
       render json: {
-        error: 'Username or password incorrect'
-      }, status: 404
+        error: 'Email or password is incorrect'
+      }, status: 422
     end
   end
+
 end
--- app/models/setlist.rb
+++ app/models/setlist.rb
@@ -1,5 +1,5 @@
 class Setlist < ApplicationRecord
   belongs_to :event
   has_many :setlist_songs
-  has_many :songs, through: :setlist_songs
+  # has_many :songs, through: :setlist_songs
 end
