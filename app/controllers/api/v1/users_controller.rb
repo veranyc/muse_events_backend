8ed2bc4f6cad185079b0820c5bf825a7c3e3ff0f
veranyc <veranyc@gmail.com>
2017-08-22 15:16:47 -0400

    updated user controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,22 +1,19 @@
 class Api::V1::UsersController < ApplicationController
-    before_action :set_user, only: [:show,:update,:delete]
 
     def index
-      users = User.all
-      render json: users, status: 200
+      @users = User.all
+      render json: @users, status: 200
     end
 
     def create
       @user = User.new(user_params)
-      @user.save
 
-      if @user.save
+      if !!@user.save
+        @user.save
         created_jwt = issue_token(id: @user.id)
         render json: { username: @user.username, jwt: created_jwt }
       else
-        render json: {
-          error: 'Username already exists'
-        }, status: 422
+        render json: {errors: @user.errors.full_messages}
       end
     end
 
@@ -38,10 +35,8 @@ class Api::V1::UsersController < ApplicationController
 
     private
     def user_params
-      params.permit(:username)
+      params.permit(:username, :password)
     end
 
-    def set_user
-      @user = User.find(params[:id])
-    end
+    
   end
