aed80378b3d3b70b3fb306f598c40b8e11196871
veranyc <veranyc@gmail.com>
2017-08-22 21:37:02 -0400

    update user table for password changes

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,9 +7,9 @@ class Api::V1::AuthController < ApplicationController
 
   def create
     user = User.find_by(username: params[:username])
-    if user.present? && user.authenticate(params[:password])
+    if (user.present?)
       created_jwt = issue_token({id: user.id})
-      render json: {username: user.username, jwt: created_jwt}
+      render json: {user: user.id, username: user.username, jwt: created_jwt}
     else
       render json: {
         error: 'Email or password is incorrect'
